4d13d0c9f182063fbb06eb35417978249fd86c13
Ivan Cherviakov <cherviakov.ivan@gmail.com>
2020-06-13 10:05:56 +0700

    initial commit

---
+++ db.py
@@ -0,0 +1,23 @@
+import os
+from string import Template
+from sqlalchemy import Column, DateTime, String, Integer, func, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Todo(Base):
+  __tablename__ = 'todo'
+  id = Column(Integer, primary_key=True)
+  label = Column(String)
+  # created_at = Column(DateTime, default=func.now())
+
+  def toDict (self):
+    return { 'id': self.id, 'label': self.label }
+
+db_url = Template('postgresql://$user:$password@$host:$port/$database')
+db_url = db_url.substitute(user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'), host=os.environ.get('DB_HOST'), port=os.environ.get('DB_PORT'), database=os.environ.get('DB_NAME'))
+engine = create_engine(db_url)
+session = sessionmaker()
+session.configure(bind=engine)
+Base.metadata.create_all(engine)
---
+++ index.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, redirect, url_for
+from mako.lookup import TemplateLookup
+from sqlalchemy.sql import insert, update, delete
+import simplejson as json
+import db
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+  lookup = TemplateLookup(directories=['templates'], strict_undefined=True)
+  template = lookup.get_template('main.html')
+  s = db.session()
+  todos = s.query(db.Todo).all()
+  parsedTodos = []
+  for todo in todos:
+    parsedTodos.append(todo.toDict())
+  return template.render(todo=parsedTodos)
+
+@app.route('/todo', methods=['HEAD', 'GET', 'POST', 'PUT', 'DELETE'])
+def todo():
+  if request.method == 'HEAD':
+    s = db.session()
+    return json.dumps(s.query(db.Todo).count())
+  elif request.method == 'GET':
+    return redirect(url_for('root'))
+  elif request.method == 'POST':
+    s = db.session()
+    s.execute(insert(db.Todo, request.form));
+    s.commit()
+    return redirect(url_for('root'))
+  elif request.method == 'PUT':
+    s = db.session()
+    s.execute(update(db.Todo).where(db.Todo.id == int(request.args.getlist('id')[0])).values(label = str(request.data, 'utf-8')))
+    s.commit()
+    return 'todo updated';
+  elif request.method == 'DELETE':
+    s = db.session()
+    s.query(db.Todo).filter(db.Todo.id == int(request.data)).delete()
+    s.commit()
+    return 'todo deleted' 
